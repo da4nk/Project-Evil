222e9448ec699b6d4673b5228251fdd59fd9859b
da4nkproplayz <chingchongplace@gmail.com>
2023-05-10 13:22:52 -0700

    menu done

--- metro/Assets/scripts/flashlight.cs
+++ metro/Assets/scripts/flashlight.cs
@@ -2,17 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class flashlight : MonoBehaviour
+public class Flashlight : MonoBehaviour
 {
     // Start is called before the first frame update
+    public new GameObject light;
+    bool LightIson = false;
+    public GameObject text;
     void Start()
     {
-        
+        light.SetActive(false);
+        text.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (LightIson == false)
+            {
+
+
+                light.SetActive(true);
+                LightIson = true;
+                
+
+                
+            }
+            else 
+            {
+
+                light.SetActive(false);
+                LightIson = false;
+
+
+            }
+        }
+
+
         
     }
 }
---
+++ metro/Assets/scripts/flashlightpickup.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class flashlightpickup : MonoBehaviour
+{
+    [SerializeField] private Transform hand;
+    public Transform flashlight;
+    public bool isHolding = false;
+    public GameObject text;
+    private void Start()
+    {
+        // Find the flashlight object by tag
+        flashlight = GameObject.FindGameObjectWithTag("Pickupable").transform;
+
+    }
+
+
+    private void Update()
+    {
+        if (!isHolding)
+        {
+            // If the player is close enough to the flashlight, pick it up
+            float distance = Vector3.Distance(transform.position, flashlight.position);
+            if(distance < 2f)
+            {
+                text.SetActive(true);
+            }
+            else if(distance > 2f)
+            {
+                text.SetActive(false);
+            }
+            if (distance < 2f && Input.GetKeyDown(KeyCode.F))
+            {
+                flashlight.SetParent(hand);
+                flashlight.localPosition = Vector3.zero;
+                flashlight.localRotation = Quaternion.identity;
+                flashlight.transform.Rotate(0f, 180f, 0f);
+                flashlight.GetComponent<Rigidbody>().isKinematic = true;
+                isHolding = true;
+            }
+        }
+
+        else
+        {
+           
+            text.SetActive(false);
+           
+            // if the player is holding the flashlight, and presses f again drop it
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+               
+
+                // makes the player no longer the parent of the flashlight
+                flashlight.SetParent(null);
+
+                // selects the kinematic body of flashlight and turns it off
+                flashlight.GetComponent<Rigidbody>().isKinematic = false;
+
+                // indicate that the flashlight is no longer held
+                isHolding = false;
+            }
+        }
+    }
+}
