499038c71eb6a7d5b476dc833e445cb717b283c0
Zyxifyrais <113056764+Zyxifyrais@users.noreply.github.com>
2023-05-17 14:22:21 -0700

    lolz idk psx filter

--- metro/Assets/scripts/movement.cs
+++ metro/Assets/scripts/movement.cs
@@ -1,28 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// movement script In c# for player movement
+
 public class movement : MonoBehaviour
 {
+    // Start is called before the first frame update
+
+    // make the player
+
+    Transform orientation;
+    public Rigidbody player;
+
+    // player walking speed
     public float speed = 11f;
-    private Rigidbody player;
-     public float radius = 0.5f;
-    public float height = 2f;
-    public int player_size = 2;
+
+
+    public Vector3 moveDirection;
 
 
     void Start()
     {
+        // select the body of player
         player = GetComponent<Rigidbody>();
+
+        // grabs the camera child from the player 
+        orientation = GetComponentInChildren<Camera>().transform;
+
     }
 
+    // Update is called once per frame
     void Update()
-    {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+    { 
+        float horizontal = 0f;
+        float vertical = 0f;
+
+    
+       
 
 
+        // movement below
+        
+        if (Input.GetKey(KeyCode.A))
+        {
 
+            horizontal = -1f;
+            
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            horizontal = 1f;
+        }
         if (Input.GetKey(KeyCode.W))
         {
             vertical = 1f;
@@ -31,42 +58,15 @@ public class movement : MonoBehaviour
         {
             vertical = -1f;
         }
-        if (Input.GetKey(KeyCode.D))
-        {
-            horizontal = 1f;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            horizontal = -1f;
-        }
 
-        float distance = speed * Time.deltaTime;
-        Vector3 localMovement = new Vector3(horizontal, 0f, vertical);
-        Vector3 globalMovement = transform.TransformDirection(localMovement).normalized;
+        // calculates the direction of the player for the movement
+        moveDirection = (orientation.TransformDirection(new Vector3(horizontal, 0, vertical))).normalized;
+        // keeps the player on the ground
+        moveDirection.y = 0;
+        
 
-        // make boolean to check if player has hit something
-
-        Vector3 position = transform.position + Vector3.up * (height / 2f - radius);
-        Vector3 direction = transform.forward;
-
-        RaycastHit hit;
-
-        // Cast the capsule and get the hit information
-        bool collided = Physics.SphereCast(transform.position, radius, transform.forward, out hit, distance);
-        bool collided2 = Physics.SphereCast(transform.position, radius, -transform.forward, out hit, distance);
-        bool collided3 = Physics.SphereCast(transform.position, radius, transform.right, out hit, distance);
-        bool collided4  = Physics.SphereCast(transform.position, radius, -transform.right, out hit, distance);
-
-
-
-
-
-
-        if ((collided || collided2 || collided3 || collided4))
-        {
-            transform.position += globalMovement * speed * Time.deltaTime;
-        }
-     
+        // applies movement to player position
+        player.transform.localPosition += moveDirection * speed * Time.deltaTime;
 
     }
 }
